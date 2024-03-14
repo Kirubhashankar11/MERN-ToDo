0c05e11a445f9e20fa0f336e9c3287d65ba12c96
kirubhashankar11~ <kirubhashankar1108@gmail.com>
2024-03-14 12:24:08 +0530

    todolist

---
+++ ToDo list/src/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Create from './Create'
+import { useState } from 'react'
+
+function Home() {
+    const[todos, setTodos] = useState([])
+  return (
+    <div className='home'> 
+    <h2>
+      ToDo List
+      
+      </h2>
+    <Create />
+    {
+      todos.length === 0 
+      ?
+      <div><h2>NO Record</h2></div>
+      :
+      todos.map(todo =>(
+        <div>
+          {todo}
+        </div>
+      ))
+    }
+    </div>
+  )
+}
+
+export default Home
---
+++ help.md
@@ -0,0 +1,14 @@
+# to install npm
+- `sudo apt install npm`
+
+# to create react template
+- ` npm create  vite@latest`
+# to install libraries
+- `npm install`
+
+# to install
+- `npm install axios react-icons`
+
+# to run
+- `npm run dev`
+
